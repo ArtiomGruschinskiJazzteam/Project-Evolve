bd7121b3e6c008a3903d394e04b76686d0d1bd08
Serega <riddik_646@mail.ru>
2017-03-18 20:40:10 +0300

    next commit Signed-off-by: Serega <riddik_646@mail.ru>

--- .idea/src/java/Main.java
+++ .idea/src/java/Main.java
@@ -2,6 +2,6 @@
 public class Main {
 int a  = 3;
     public static void main (String [] args){
-        System.out.println("Yasuo musor!!!!#!$!!$!!!!!2!");
+        System.out.println("Yasuo musor!!!!#!$!!!$!!!!!2!");
     }
 }
